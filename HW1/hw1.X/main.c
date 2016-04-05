22baa42b9eefbc220d9a5a6acbe8759e5ec59e00
alshorte <amandashorter3@gmail.com>
2016-04-05 14:48:43 -0500

    program complete

--- HW1/hw1.X/main.c
+++ HW1/hw1.X/main.c
@@ -54,12 +54,26 @@ int main() {
     DDPCONbits.JTAGEN = 0;
     
     // do your TRIS and LAT commands here
+    TRISB =  0xFFFF; // pin 4 of Port B is Button set to one for input (reset value = FFFF)
+    TRISAbits.TRISA4 = 0; // pin 4 of Port A is LED set to zero for output (reset value = 079F)
+
+    
+    LATAbits.LATA4 = 0;  // initiallize LED to be on
     
     __builtin_enable_interrupts();
     
     while(1) {
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
-		// remember the core timer runs at half the CPU speed
+        // remember the core timer runs at half the CPU speed
+        
+            _CP0_SET_COUNT(0);   // set core timer to 0
+            while (_CP0_GET_COUNT() < 12002){ // running at 48 MHz, Core timer at 24 MHZ
+                ; // wait 0.5 ms
+                while(!PORTBbits.RB4) { 
+                    ;
+               } // Pin B4 is the button, low (FALSE) if pressed.
+            }
+            LATAINV = 0b10000 ;  // toggle LED pin 4
     }
     
     
