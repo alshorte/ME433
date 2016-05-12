cecebf6561b568677fa9764c2a9b7d785be93e3f
alshorte <amandashorter3@gmail.com>
2016-05-12 15:42:49 -0500

    remove unnecessary functions from hw4 used for debugging hw6

--- HW4/hw4.X/LCD.h
+++
@@ -1,9 +0,0 @@
-#ifndef LCD_H
-#define	LCD_H
-
-#include "ILI9163C.h"       //LCD functions
-void draw_character(unsigned char c, unsigned char x, unsigned char y);
-void draw_string(unsigned char *m, unsigned char x, unsigned char y);
-
-#endif	/* LCD_H */
-
